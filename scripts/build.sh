adbd8d0e39a2ff62dd2bd7044ef032b703ac748b
ringeringeraja <joaosan177@gmail.com>
2022-04-17 17:38:31 -0300

    gitignore

--- scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-BUILD_COMPONENTS=
-
-npm run build && \
-  cp -r packages/backend/src/resources/* dist/backend/src/resources && \
-  cp packages/backend/RELEASE.yml dist/backend/ 2>/dev/null && \
-  [ ! -z $BUILD_COMPONENTS ] && (cd frontend && npm run build)
