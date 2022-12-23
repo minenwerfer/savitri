c519c26d6750663330c2505e766dcc5b81b4d810
ringeringeraja <joaosan177@gmail.com>
2022-12-23 02:09:54 -0300

    fix

--- examples/api-headless/packages/index.ts
+++ examples/api-headless/packages/index.ts
@@ -1,5 +1,22 @@
+import type { ApiContext } from '@savitri/api'
 import { init } from '@savitri/api/server'
 
-init().then(server => {
+const context: Partial<ApiContext> = {
+  apiConfig: {
+    roles: {
+      guest: {
+        capabilities: {
+          person: {
+            methods: [
+              'hello'
+            ]
+          }
+        }
+      }
+    }
+  }
+}
+
+init({ context }).then(server => {
   server.start()
 })
--- examples/api-ionic/packages/api/index.ts
+++ examples/api-ionic/packages/api/index.ts
@@ -1,22 +1,5 @@
-import type { ApiContext } from '@savitri/api'
 import { init } from '@savitri/api/server'
 
-const context: Partial<ApiContext> = {
-  apiConfig: {
-    roles: {
-      guest: {
-        capabilities: {
-          person: {
-            methods: [
-              'hello'
-            ]
-          }
-        }
-      }
-    }
-  }
-}
-
-init({ context }).then(server => {
+init().then(server => {
   server.start()
 })
