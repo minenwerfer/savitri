81b8a0bf7bee802add3c0aeeff133f87a9343018
ringeringeraja <joaosan177@gmail.com>
2023-05-11 23:55:45 -0300

    useNavbar

--- packages/ui/components/dashboard/sv-navbar/sv-navbar.vue
+++ packages/ui/components/dashboard/sv-navbar/sv-navbar.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-import { ref, computed, watch, } from 'vue'
-import { arraysIntersects } from '@semantic-api/common'
-import { useStore, useRouter, Route, MenuSchema } from '@savitri/web'
+import { useRouter, useNavbar, Route, MenuSchema } from '@savitri/web'
 import SvIcon from '../../sv-icon/sv-icon.vue'
 import SvNavbarHeader from './_internals/components/sv-navbar-header/sv-navbar-header.vue'
 
@@ -23,9 +21,6 @@ type Emit = {
 
 const props = defineProps<Props>()
 const emit = defineEmits<Emit>()
-
-const metaStore = useStore('meta')
-const userStore = useStore('user')
 const router = await useRouter()
 
 const onEntryClick = (route: Route & { meta: any }) => {
@@ -41,78 +36,12 @@ const onEntryClick = (route: Route & { meta: any }) => {
   }
 }
 
-const getSchema = (schema: any, routes: Array<Route>): Array<Route&SchemaNode> => {
-  if( !Array.isArray(schema) ) {
-    return schema
-  }
+const {
+  isCurrent,
+  routes,
+  routesWithChildren
 
-  return schema.map((s) => {
-    return typeof s === 'string'
-      ? routes.find((route) => route.name === s)
-      : {
-        ...s,
-        ...routes.find((route) => route.name === s.name)
-      }
-  })
-}
-
-const getRoutes = (node?: SchemaNode): Array<Route> => {
-  const children = node?.children
-  const routes: unknown = children || typeof props.entrypoint === 'string'
-    ? router.getRoutes().filter((route) => (route.name as string ||'').startsWith(`/${props.entrypoint}/`))
-    : router.getRoutes() 
-
-  const schema = getSchema(children || props.schema, routes as Array<Route>)
-  const entries: Record<string, Route> = {}
-
-  Object.entries(schema).forEach(([key, node]) => {
-    if( !node ) {
-      return
-    }
-
-    const {
-      children,
-      ...route
-
-    } = node
-
-    const roles = route?.meta?.roles || node.roles
-    if( roles && !arraysIntersects(userStore.$currentUser.roles, roles) ) {
-      return
-    }
-
-    entries[key] = route
-    entries[key].meta = route.meta || {
-      title: key
-    }
-
-    if( children ) {
-      entries[key].children = getRoutes(node)
-    }
-  })
-
-  return Object.values(entries) as Array<Route>
-}
-
-const isCurrent = (subroute: any) => {
-  const route = router.currentRoute.value
-
-  const pathMatches = typeof subroute.redirect === 'string'
-    ? subroute.redirect === route.path
-    : subroute.path === (route.redirectedFrom?.path || route.path)?.split(/\/home$/)[0]
-
-  const nameMatches = subroute.name === (route.redirectedFrom?.name || route.name)
-  return pathMatches || nameMatches
-}
-
-const routes = ref<Array<Route>>(getRoutes())
-const routesWithChildren = computed(() => (
-  routes.value.filter((route) => route.children?.length! > 0)
-))
-
-watch(() => metaStore.descriptions, () => {
-  routes.value = getRoutes()
-})
+} = await useNavbar(props)
 </script>
 
 <template>
---
+++ packages/web/src/composables/use-navbar.ts
@@ -0,0 +1,98 @@
+import { ref, computed, watch, } from 'vue'
+import { arraysIntersects } from '@semantic-api/common'
+import { useStore, useRouter, Route, MenuSchema } from '..'
+
+type SchemaNode = {
+  roles?: Array<string>
+  children?: Route
+}
+
+type Props = {
+  entrypoint?: string
+  schema: MenuSchema
+}
+
+export const useNavbar = async (props: Props) => {
+  const metaStore = useStore('meta')
+  const userStore = useStore('user')
+  const router = await useRouter()
+
+  const getSchema = (schema: any, routes: Array<Route>): Array<Route&SchemaNode> => {
+    if( !Array.isArray(schema) ) {
+      return schema
+    }
+
+    return schema.map((s) => {
+      return typeof s === 'string'
+        ? routes.find((route) => route.name === s)
+        : {
+          ...s,
+          ...routes.find((route) => route.name === s.name)
+        }
+    })
+  }
+
+  const getRoutes = (node?: SchemaNode): Array<Route> => {
+    const children = node?.children
+    const routes: unknown = children || typeof props.entrypoint === 'string'
+      ? router.getRoutes().filter((route: Route) => (route.name as string ||'').startsWith(`/${props.entrypoint}/`))
+      : router.getRoutes() 
+
+    const schema = getSchema(children || props.schema, routes as Array<Route>)
+    const entries: Record<string, Route> = {}
+
+    Object.entries(schema).forEach(([key, node]) => {
+      if( !node ) {
+        return
+      }
+
+      const {
+        children,
+        ...route
+
+      } = node
+
+      const roles = route?.meta?.roles || node.roles
+      if( roles && !arraysIntersects(userStore.$currentUser.roles, roles) ) {
+        return
+      }
+
+      entries[key] = route
+      entries[key].meta = route.meta || {
+        title: key
+      }
+
+      if( children ) {
+        entries[key].children = getRoutes(node)
+      }
+    })
+
+    return Object.values(entries) as Array<Route>
+  }
+
+  const isCurrent = (subroute: any) => {
+    const route = router.currentRoute.value
+
+    const pathMatches = typeof subroute.redirect === 'string'
+      ? subroute.redirect === route.path
+      : subroute.path === (route.redirectedFrom?.path || route.path)?.split(/\/home$/)[0]
+
+    const nameMatches = subroute.name === (route.redirectedFrom?.name || route.name)
+    return pathMatches || nameMatches
+  }
+
+  const routes = ref<Array<Route>>(getRoutes())
+  const routesWithChildren = computed(() => (
+    routes.value.filter((route) => route.children?.length! > 0)
+  ))
+
+  watch(() => metaStore.descriptions, () => {
+    routes.value = getRoutes()
+  })
+
+  return {
+    routes,
+    routesWithChildren,
+    isCurrent
+  }
+}
