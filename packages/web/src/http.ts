ba3cc0fe6fc535d353a2d6da684e3207d14b2a37
ringeringeraja <joaosan177@gmail.com>
2023-07-19 01:10:17 -0300

    feat(web): behavior change: exceptions dont trigger a modal by default anymore

--- packages/web/src/http.ts
+++ packages/web/src/http.ts
@@ -19,19 +19,10 @@ const call = (target: typeof proxiedHttp | typeof request) => <Return=any>(...ar
 
 const proxiedHttp = async <Return>(...args: Parameters<typeof request<any>>) => {
   return request<Return>(...args).catch((error: any) => {
-    const metaStore = useStore('meta')
     const userStore = useStore('user')
-
     if( error.logout || ['JsonWebTokenError', 'TokenExpiredError'].includes(error.name) ) {
       userStore.signout()
       ROUTER.push({ name: '/user/signin' })
-    }
-
-    if( !error.silent ) {
-      metaStore.spawnModal({
-        title: 'Error',
-        body: error
-      })
     }
 
     console.trace(error)
